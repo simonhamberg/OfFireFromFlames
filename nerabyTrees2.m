6ffed3ed030673fa373baa02727b327ce8883b44
GVevang <118185280+GVevang@users.noreply.github.com>
2022-11-21 14:33:50 +0100

    Update nerabyTrees2.m

--- nerabyTrees2.m
+++ nerabyTrees2.m
@@ -1,12 +1,10 @@
-function sortedVector = nerabyTrees2(m,i,j,criticalRadius)
-sortedVector=zeros(2,length(m)); %x,y
+function sortedVector = nerabyTrees2(m,i,criticalRadius)
+sortedVector=zeros(1,length(m)); %i
 sortedVector(1,1) = i;
-sortedVector(2,1) = j;
 sortedVectorIterator = 2;
 for i1 = 1:length(m)
-    if( abs(m(i1,j) ) <= criticalRadius)
+    if( abs(m(i1,i) ) <= criticalRadius)
             sortedVector(1,sortedVectorIterator) = i1;
-            sortedVector(2,sortedVectorIterator) = j;
             sortedVectorIterator = sortedVectorIterator + 1;
     end
 end
