7c691b3f047e6d73023eeb212163e1ae0fad37d5
TurboFarfar <118185123+TurboFarfar@users.noreply.github.com>
2022-11-21 14:17:24 +0100

    Add files via upload

---
+++ CalcDist.m
@@ -0,0 +1,22 @@
+%Calculate distance
+%Course [FFR120]
+%Group Anacondas
+%--------------------------------------------------------------------------
+%--------------------------------------------------------------------------
+%Author: Tim Johansson
+clc; clear; clf;
+
+forestPos = readmatrix('Forest.csv');
+
+[n,N] = size(forestPos);
+distance = zeros(N,N);
+
+%Create a distance matrix
+for i = 1:N
+    for j = 1:N
+        distance(i,j) = getDistance(forestPos(:,i),forestPos(:,j));
+    end
+end
+
+%csvwrite('DistanceMatrix.csv', distance);
+save('DistanceMatrix.csv', distance);
---
+++ getDistance.m
@@ -0,0 +1,5 @@
+function distance = getDistance(pos1,pos2)
+    %Distance between pos1 and pos2
+    distance = sqrt((pos1(1) - pos2(1))^2 + (pos1(2) - pos2(2))^2);
+end
+
