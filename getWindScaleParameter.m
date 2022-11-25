b99b97e28a49080f96f8ebb090c567177fe47a1e
OscarK31 <118184285+OscarK31@users.noreply.github.com>
2022-11-25 16:09:11 +0100

    Update getWindScaleParameter.m

--- getWindScaleParameter.m
+++ getWindScaleParameter.m
@@ -5,10 +5,7 @@
 %--------------------------------------------------------------------------
 %Author: Oscar Karlsson
 
-function windScale  = getWindScaleParameter(tree1,tree2,windAngle, windStrength)
-
-%Angle between two trees in radians
-treeAngle = atan((tree2(2)-tree1(2))/(tree2(1)-tree1(1)));
+function windScale  = getWindScaleParameter(treeAngle,windAngle, windStrength)
 
 if (abs(treeAngle-windAngle) < pi/2)
     %Tree is in the wind generall direction -> increase likelyhood of
