11b8d07ede2def23e8b2c5e8b8e3e74b3a4ffba3
GVevang <118185280+GVevang@users.noreply.github.com>
2022-11-25 14:37:27 +0100

    Update nerabyTrees2.m
    Clearer variable names and fixed an error where the burning tree is returned twice. Returns a vector of indicies of nearby trees. First indicie is the input tree.

--- nerabyTrees2.m
+++ nerabyTrees2.m
@@ -1,10 +1,12 @@
-function sortedVector = nerabyTrees2(m,i,criticalRadius)
-sortedVector=zeros(1,length(m)); %i
-sortedVector(1,1) = i;
+%%Author Gabriel Vevang
+function sortedVector = nerabyTrees2(distanceMatrix,chosenTreeIndex,criticalRadius)
+sortedVector=zeros(1,length(distanceMatrix)); 
+sortedVector(1,1) = chosenTreeIndex;
 sortedVectorIterator = 2;
-for i1 = 1:length(m)
-    if( abs(m(i1,i) ) <= criticalRadius)
+for i1 = 1:length(distanceMatrix)
+    if( abs(distanceMatrix(i1,chosenTreeIndex) ) <= criticalRadius && i1 ~= chosenTreeIndex)
             sortedVector(1,sortedVectorIterator) = i1;
             sortedVectorIterator = sortedVectorIterator + 1;
     end
 end
+sortedVector = sortedVector(1:sortedVectorIterator-1);
