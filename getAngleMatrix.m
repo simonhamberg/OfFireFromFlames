1a21838f14981c78c47b31f6b4552d9fb067c020
GVevang <118185280+GVevang@users.noreply.github.com>
2022-11-25 16:32:23 +0100

    Update getAngleMatrix.m

--- getAngleMatrix.m
+++ getAngleMatrix.m
@@ -1,3 +1,4 @@
-function windScale  = getAngleMatrix(tree1,tree2)
+%Author Oscar Karlsson
+function treeAngle  = getAngleMatrix(tree1,tree2)
     treeAngle = atan((tree2(2)-tree1(2))/(tree2(1)-tree1(1)));
 end
