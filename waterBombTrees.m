4039e381f1392fc3a730c83112b6f049bc072b65
JohanRumarKarlquist <118185012+JohanRumarKarlquist@users.noreply.github.com>
2022-11-28 16:33:37 +0100

    Update waterBombTrees.m

--- waterBombTrees.m
+++ waterBombTrees.m
@@ -1,17 +1,12 @@
 %Author Johan Rumar Karlquist
 function indexWaterBombTrees = waterBombTrees(lineStart,lineEnd,forestPos,bombRadius)
-    distanceToLine=ones(5000,1)*(bombRadius+1);
+    lineDistance=ones(5000,1)*(bombRadius+1);
     indexWaterBombTrees=[];
     for i=1:5000
-        numerator = abs((lineEnd(1) - lineStart(1)) * (lineStart(2) - forestPos(2,i))... 
-        - (lineStart(1) - forestPos(1,i)) * (lineEnd(2) - lineStart(2)));
- 
-        denominator = sqrt((lineEnd(1) - lineStart(1)) ^ 2 + (lineEnd(2) - lineStart(2)) ^ 2);
-        
-        distanceToLine(i)=numerator./denominator;
+        lineDistance(i)=distanceToLine(forestPos(:,i)',lineStart,lineEnd);
     end
     for i=1:5000
-        if distanceToLine(i)<bombRadius
+        if lineDistance(i)<bombRadius
             indexWaterBombTrees(length(indexWaterBombTrees)+1)=i;
         end
     end
