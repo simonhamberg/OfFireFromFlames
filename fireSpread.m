98296a8341f85731e0adf503bccb7fe7009a6b27
OscarK31 <118184285+OscarK31@users.noreply.github.com>
2022-12-02 09:05:26 +0100

    Update fireSpread.m

--- fireSpread.m
+++ fireSpread.m
@@ -1,17 +1,15 @@
 %%Author Gabriel Vevang
 
 function burningTreeVector = fireSpread(burningTree,distanceMatrix,probabilityConstant,criticalRadius,Wind)
-nearbyTrees = nerabyTrees2(distanceMatrix,burningTree,criticalRadius);
-burningTreeVector = zeros(1,length(nearbyTrees));
-%probabilityConstant = 5; %Constant, currently 0.2 if tree 5 meters away
-i2 = 1;
-for i = 1:length(nearbyTrees)-1
-    distance = distanceMatrix(burningTree,nearbyTrees(i+1));
-    %windImpact = Wind(burningTree,nearbyTrees(i+1));
-    Probability = 1/(distance)^2*probabilityConstant*Wind;%windImpact; 
-    if (Probability > rand)
-        burningTreeVector(i2) = nearbyTrees(i2+1);
-        i2 = i2 + 1;
+    nearbyTrees = nerabyTrees2(distanceMatrix,burningTree,criticalRadius);
+    burningTreeVector = zeros(1,length(nearbyTrees));
+    i2 = 1;
+    for i = 1:length(nearbyTrees)-1
+        distance = distanceMatrix(burningTree,nearbyTrees(i+1));
+        Probability = 1/(distance)^2*probabilityConstant*Wind; 
+        if (Probability > rand)
+            burningTreeVector(i2) = nearbyTrees(i2+1);
+            i2 = i2 + 1;
+        end
     end
-end
-burningTreeVector = burningTreeVector(1:i2-1);
+    burningTreeVector = burningTreeVector(1:i2-1);
