c82c4459c28a1a8223b9020ca4ef09320f12fe73
OscarK31 <118184285+OscarK31@users.noreply.github.com>
2022-12-02 09:02:47 +0100

    Update Main.m

--- Main.m
+++ Main.m
@@ -15,10 +15,12 @@ distanceMat = readmatrix('DistanceMatrix.csv');
 %--------------------------------------------------------------------------
 %Author: Tim Johansson
 %Local parameters
+
 clf;
 %Comment out either angleMatrix on the row below or the row that creates it
-%further down
-clearvars -except forestPos distanceMat %angleMatrix
+%further down after you've loaded these 3 matrices
+clearvars -except forestPos distanceMat
+
 treeRadius = 2;
 forestSize = [1000,1000];
 treeOffset = 0;
@@ -29,8 +31,7 @@ probabilityConstant = criticalRadius^2/2 ;
 
 [n,N] = size(forestPos);
 plot(forestPos(1,:),forestPos(2,:),'.','color',[0 100/255 0],'MarkerSize',treeRadius*2);
-isBurning = false(N,1);
-isWater = false(N,1);
+isBurning = zeros(N,1); %%false
 
 windScaleParam = zeros(N,N);
 
@@ -38,25 +39,26 @@ windScaleParam = zeros(N,N);
 windAngle = pi/6;
 windAngleAlterations = 1;
 
-windStrength = 100.*rand; %Rand strength between 0 and 3;
+windStrength = 100.*rand;
 windStrengthAlterations = 10;
 
 [x,y] = ginput(1);
 a = [x,y];
 d = zeros(N,1);
 
-angleMatrix = getAngleMatrix(a, forestPos);
+%Get all trees angles relative to the start position
+angleMatrix = getAngleMatrix(a,forestPos);
 
 for i = 1:N
     d(i) = norm(forestPos(:,i) - a');
     if d(i) <= initFireRadius
-        isBurning(i) = true;
+        isBurning(i) = 5;
     end
 end
 
 hold on
 for i = 1:N
-    if isBurning(i)
+    if isBurning(i) > 0
         plot(forestPos(1,i),forestPos(2,i),'.','color',1/255*[255 130 0],'MarkerSize',treeRadius*2);
     end
 end
@@ -66,24 +68,25 @@ simFrames = 1000; %Number of iterations
 waterStart = 100; %Starts after "waterStart" iterations
 waterStop = 150;
 waterBombXpos = 100;
-wasBurning = false(N,1);
+%wasBurning = false(N,1);
 r_gb = 120;
 for iteration = 1:simFrames
     pause(0.0001);
-    wasBurning = isBurning + wasBurning;
+    
+    %wasBurning = isBurning + wasBurning;
 
-    %Wind calculations
+    %Wind alternations
     if mod(iteration,windAngleAlterations) == 0
         deltaAngle =  normrnd(0,pi/6);
         windAngle = windAngle + deltaAngle;
     end
 
-    %Alternate windStrength a bit more often than windAngle
+    %Windstrength alternations
      if mod(iteration,windStrengthAlterations) == 0
-        %Create random small strength alteration
         deltaStrength = (2*rand(1,1)-1)/windStrengthAlterations;
 
-        %Limit strength to 3x
+        %Limit strength (Compared "Fresh breeze" to "Strong gale", see The Beaufort Wind Scale)
+        %Wind speed would be a more accurate name
         if (windStrength + deltaStrength) < 1/3 * probabilityConstant
             windStrength = 1/3 * probabilityConstant;
         elseif (windStrength + deltaStrength) > 3 * probabilityConstant
@@ -105,36 +108,24 @@ for iteration = 1:simFrames
     %     newBurnetTrees = false(1,trees);
     newBurnetTrees = isBurning;
     for i = 1:N
-        if isBurning(i)
+        if isBurning(i) > 0
             temp = fireSpread(i,distanceMat,probabilityConstant,criticalRadius,windMatrix(i,1));
+            newBurnetTrees(i) = newBurnetTrees(i) - 2;
             for i1 = 1:length(temp)
-                newBurnetTrees(temp(i1),1) = true;
+                if (isBurning(temp(i1)) == 0)
+                    newBurnetTrees(temp(i1),1) = 5;
+                end
             end
         end
     end
     
-    newBurnetTrees = newBurnetTrees - isBurning;
+    %newBurnetTrees = newBurnetTrees - isBurning;
     isBurning = newBurnetTrees;
     
 
 
 
-    if mod(iteration,5)==0 %Gör vattenbombning varje 5 iterationer (subject to change)
-        indexBurningTrees=[];
-        for i=1:N
-            if isBurning(i)
-                indexBurningTrees(length(indexBurningTrees)+1)=i;
-            end
-        end
-        indexRandomBurningTree=indexBurningTrees(randn([1,length(indexBurningTrees)]));
-        [lineEnd,lineStart]=getWaterBombDirection(forestPos(1,indexRandomBurningTree),a(1),forestPos(2,indexRandomBurningTree),a(2)); 
-        %x1,y1 är koordinater för ett träd som nyss börjat brinna och x2,y2 är koordinater för eldens start position. Vet inte exakt hur man får det än.
-        waterTrees=waterBombTrees(lineStart,lineEnd,forestPos,50);
-        for i =1:length(waterTrees)
-            isWater(waterTrees(i))=true;
-            isBurning(waterTrees(i))=false;
-        end
-    end
+
 
     %Graphics
     %     if iteration > waterStart && iteration < waterStop
@@ -149,28 +140,28 @@ for iteration = 1:simFrames
     %         plot(10,abs(coords),'.','color','b');
     %     end
     %
-    changeInForest = false;
-    for i = 1:N 
+  changeInForest = false;
+    for i = 1:N
         
-        if isBurning(i) && wasBurning(i) == false
-            wasBurning(i,simFrames + 1) = isBurning(i);
-            if r_gb<0
-                r_gb = 0;
+        if isBurning(i) > 0 %&& wasBurning(i) == false
+           % wasBurning(i,simFrames + 1) = isBurning(i);
+%             if r_gb<0
+%                 r_gb = 0;
+%             end
+            if isBurning(i) == 5
+                plot(forestPos(1,i),forestPos(2,i),'.','color',1/255*[255 0 0],'MarkerSize',treeRadius*2);  
+            elseif isBurning(i) == 3      
+                plot(forestPos(1,i),forestPos(2,i),'.','color',1/255*[255 160 0],'MarkerSize',treeRadius*2);  
+            elseif isBurning(i) == 1 
+                plot(forestPos(1,i),forestPos(2,i),'.','color',[0 0 0],'MarkerSize',treeRadius*2); 
             end
-            plot(forestPos(1,i),forestPos(2,i),'.','color',1/255*[255 r_gb 0],'MarkerSize',treeRadius*2);
             changeInForest = true;
-        end
-        
-        if wasBurning(i)
+        elseif isBurning(i) < 0
             plot(forestPos(1,i),forestPos(2,i),'.','color',[.7 .7 .7],'MarkerSize',treeRadius*2);
         end       
-        %Lägg till plot för vattnade träd.
-        %if isWater(i)
-        %    plot(forestPos(1,i),forestPos(2,i),'.','color','b','MarkerSize',treeRadius*2);
-        %end
         
     end
-    r_gb = r_gb - 25;
+    %r_gb = r_gb - 25;
     iteration
     if ~changeInForest %added
         disp('No more trees are burning')
