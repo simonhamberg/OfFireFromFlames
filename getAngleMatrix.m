d5b0439aadac261203b64b58e8197739a8f71c83
OscarK31 <118184285+OscarK31@users.noreply.github.com>
2022-12-02 07:27:07 +0100

    Update getAngleMatrix.m

--- getAngleMatrix.m
+++ getAngleMatrix.m
@@ -1,10 +1,7 @@
 % %Author Oscar Karlsson
-function treeAngle = getAngleMatrix(a)
-forestPos = readmatrix('Forest.csv');
-%a = [500,500];
+function treeAngle = getAngleMatrix(a,forestPos)
 [n,N] = size(forestPos);
 treeAngle = zeros(N,1);
-
 %Create a distance matrix
 for i = 1:N
         treeAngle(i,1) = atan2((forestPos(2,i)-a(2)),(forestPos(1,i)-a(1)));
