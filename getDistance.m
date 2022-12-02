4ac1e7daab4f7ee501658ce5add9176f3a660a63
OscarK31 <118184285+OscarK31@users.noreply.github.com>
2022-12-02 09:06:29 +0100

    Update getDistance.m

--- getDistance.m
+++ getDistance.m
@@ -1,5 +1,5 @@
 function distance = getDistance(pos1,pos2)
-    %Distance between pos1 and pos2
+    %Distance between position 1 and position 2
     distance = sqrt((pos1(1) - pos2(1))^2 + (pos1(2) - pos2(2))^2);
 end
 
