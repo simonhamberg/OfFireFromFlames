f505ba2a39dc69fc0e2764ab50f4042520739d31
OscarK31 <118184285+OscarK31@users.noreply.github.com>
2022-12-02 09:07:42 +0100

    Update nerabyTrees2.m

--- nerabyTrees2.m
+++ nerabyTrees2.m
@@ -1,12 +1,13 @@
 %%Author Gabriel Vevang
 function sortedVector = nerabyTrees2(distanceMatrix,chosenTreeIndex,criticalRadius)
-sortedVector=zeros(1,length(distanceMatrix)); 
-sortedVector(1,1) = chosenTreeIndex;
-sortedVectorIterator = 2;
-for i1 = 1:length(distanceMatrix)
-    if( abs(distanceMatrix(i1,chosenTreeIndex) ) <= criticalRadius && i1 ~= chosenTreeIndex)
-            sortedVector(1,sortedVectorIterator) = i1;
-            sortedVectorIterator = sortedVectorIterator + 1;
+    
+    sortedVector=zeros(1,length(distanceMatrix)); 
+    sortedVector(1,1) = chosenTreeIndex;
+    sortedVectorIterator = 2;
+    for i1 = 1:length(distanceMatrix)
+        if( abs(distanceMatrix(i1,chosenTreeIndex) ) <= criticalRadius && i1 ~= chosenTreeIndex)
+                sortedVector(1,sortedVectorIterator) = i1;
+                sortedVectorIterator = sortedVectorIterator + 1;
+        end
     end
-end
-sortedVector = sortedVector(1:sortedVectorIterator-1);
+    sortedVector = sortedVector(1:sortedVectorIterator-1);
