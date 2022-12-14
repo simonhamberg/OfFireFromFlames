d751d8b2e82328b1bb3e12ca2e3870efd0b60bb0
GVevang <118185280+GVevang@users.noreply.github.com>
2022-12-14 11:25:38 +0100

    Update fireSpread.m

--- fireSpread.m
+++ fireSpread.m
@@ -6,7 +6,7 @@ function burningTreeVector = fireSpread(burningTree,distanceMatrix,probabilityCo
     i2 = 1;
     for i = 1:length(nearbyTrees)-1
         distance = distanceMatrix(burningTree,nearbyTrees(i+1));
-        Probability = 1/(distance)^2*probabilityConstant*Wind; 
+        Probability = 1/(distance)^4*probabilityConstant*Wind; 
         if (Probability > rand)
             burningTreeVector(i2) = nearbyTrees(i+1);
             i2 = i2 + 1;
